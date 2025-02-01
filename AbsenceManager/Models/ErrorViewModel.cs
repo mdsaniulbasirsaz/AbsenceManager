e78003d9a11595bd9f1e7158cfefb48032da199a
mdsaniulbasirsaz <saniulsaj@gmail.com>
2025-02-01 22:22:10 +0600

    Add project files.

---
+++ AbsenceManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AbsenceManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ AbsenceManager/Views/Home/Apply.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbsenceManager.Views.Home
+{
+    public class ApplyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
